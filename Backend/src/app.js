9d60d03aa1b5cc470ce81bc54f44c2e542c53b62
Adithyavkotian <adithyavkotian@gmail.com>
2025-03-08 00:17:04 +0530

    Initial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes');
+const cors=require('cors')
+
+
 const app = express();
 
+
+app.use(cors())
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,59 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
 import Prism from "prismjs";
+import "prismjs/components/prism-jsx.min";
+import axios from 'axios';
 import './App.css';
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 function App() {
+  const [code, setCode] = useState(`function add(a, b) {
+  return a + b;
+}`);
+
+  const [review, setReview] = useState('');
+
   useEffect(() => {
     Prism.highlightAll();
-  }, []);
+  }, [code]);
+
+  async function reviewCode() {
+    try {
+      const response = await axios.post('http://localhost:3000/ai/generate-content', { prompt: code });
+      setReview(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
   return (
     <>
       <main>
         <div className="left">
           <div className="code">
-            <pre>
-              <code className="language-javascript">
-                {`
-                  function add() {
-                    return 1 + 1;
-                  }
-                `}
-              </code>
-            </pre>
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => Prism.highlight(code, Prism.languages.jsx, 'jsx')}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+               
+              }}
+            />
           </div>
-          <div className="review-btn">
+          <div onClick={reviewCode} className="review-btn">
             Review
           </div>
         </div>
         <div className="right">
-          {/* Add content here */}
+          <Markdown rehypePlugins={[rehypeHighlight]}>
+            {review}
+          </Markdown>
         </div>
       </main>
     </>
